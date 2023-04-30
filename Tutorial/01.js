96ffaff87639d1b4adf17a845a315cf8f50f6ffd
Sonu Mondal <takeinfo2u@gmail.com>
2023-04-30 16:05:21 +0530

    I started tracking all the files and folders

---
+++ Tutorial/01.js
@@ -0,0 +1,9 @@
+let score=324;
+console.log(score);
+let coursePrice=300
+let gstOnCourse=36
+let totalAmountOfCourse=coursePrice+gstOnCourse
+
+console.log("The total price of course is:",totalAmountOfCourse);
+
+console.log("sonu"+score+coursePrice+gstOnCourse+totalAmountOfCourse); // string concatanate eg
